a64c474b144ab79006280787043ef788e21a1b28
PA-AIT <153344795+PA-AIT@users.noreply.github.com>
2023-12-10 22:19:28 +0530

    Update Shin.py

--- Shin.py
+++ Shin.py
@@ -27,6 +27,9 @@ if not user or not password:
     st.error("Please enter both email address and password.")
     st.stop()
 
+# Button for triggering summarization
+summarize_button = st.button("Summarize Emails")
+
 # IMAP connection and email processing
 imap_url = 'imap.gmail.com'
 try:
@@ -43,35 +46,37 @@ try:
         value = pdf_email_address
         date_criteria = f'SINCE "{date_search_criteria}"'  # Corrected the date format
 
-        _, data = my_mail.search(None, key, value, date_criteria)
-        mail_id_list = data[0].split()
+        # Display summary only if the button is clicked
+        if summarize_button:
+            _, data = my_mail.search(None, key, value, date_criteria)
+            mail_id_list = data[0].split()
 
-        info_list = []
-        summarizer = pipeline("summarization")
+            info_list = []
+            summarizer = pipeline("summarization")
 
-        for num in mail_id_list:
-            typ, data = my_mail.fetch(num, '(RFC822)')
-            msg = email.message_from_bytes(data[0][1])
+            for num in mail_id_list:
+                typ, data = my_mail.fetch(num, '(RFC822)')
+                msg = email.message_from_bytes(data[0][1])
 
-            book_title = msg["Subject"]
-            email_date = msg["Date"]
+                book_title = msg["Subject"]
+                email_date = msg["Date"]
 
-            for part in msg.walk():
-                if part.get_content_type() == 'application/pdf':
-                    pdf_bytes = part.get_payload(decode=True)
-                    pdf_text = extract_text_from_pdf(pdf_bytes)
+                for part in msg.walk():
+                    if part.get_content_type() == 'application/pdf':
+                        pdf_bytes = part.get_payload(decode=True)
+                        pdf_text = extract_text_from_pdf(pdf_bytes)
 
-                    # Increase the max_length for better summarization
-                    max_length = min(len(pdf_text) * 2, 1024)  # Limit max_length to avoid very long summaries
-                    summarized_content = summarizer(pdf_text, max_length=max_length)[0]['summary']
+                        # Increase the max_length for better summarization
+                        max_length = min(len(pdf_text) * 2, 1024)  # Limit max_length to avoid very long summaries
+                        summarized_content = summarizer(pdf_text, max_length=max_length)[0]['summary']
 
-                    info = {"Book Title": book_title, "Received Date": email_date,
-                            "Summarized Content": summarized_content}
-                    info_list.append(info)
+                        info = {"Book Title": book_title, "Received Date": email_date,
+                                "Summarized Content": summarized_content}
+                        info_list.append(info)
 
-        for info in info_list:
-            st.subheader(f"{info['Book Title']} - Received Date: {info['Received Date']}")
-            st.write(info["Summarized Content"])
+            for info in info_list:
+                st.subheader(f"{info['Book Title']} - Received Date: {info['Received Date']}")
+                st.write(info["Summarized Content"])
 
 except imaplib.IMAP4.error as e:
     st.error(f"IMAP error occurred: {str(e)}")
